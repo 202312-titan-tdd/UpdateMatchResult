8891c7add1bf92dd75b7698ac9a701c2218a5d5a
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 12:00:45 +0800

    refactor: test

--- OneBackTests/MatchControllerTests.cs
+++ OneBackTests/MatchControllerTests.cs
@@ -14,19 +14,39 @@ namespace OneBackTests;
 [TestFixture]
 public class MatchControllerTests
 {
-    [Test]
-    public void home_goal()
+    private MatchController _matchController = null!;
+    private IMatchRepo _matchRepo = null!;
+
+    [SetUp]
+    public void SetUp()
     {
-        var matchRepo = Substitute.For<IMatchRepo>();
-        var matchController = new MatchController(matchRepo);
+        _matchRepo = Substitute.For<IMatchRepo>();
+        _matchController = new MatchController(_matchRepo);
+    }
 
-        matchRepo.GetMatch(91)
-                 .Returns(new Match { Id = 91, MatchResult = new MatchResult("") });
+    [Test]
+    public void home_goal_when_0_0_first_half()
+    {
+        GivenMatchResultFromRepo("");
+        AfterActionDisplayResultShouldBe(EnumAction.HomeGoal, "1:0 (First Half)");
+        ShouldUpdateMatchResult("H");
+    }
 
-        var updateMatchResult = matchController.UpdateMatchResult(91, EnumAction.HomeGoal);
-        updateMatchResult.Should().Be("1:0 (First Half)");
+    private void ShouldUpdateMatchResult(string matchResult)
+    {
+        _matchRepo.Received()
+                  .UpdateMatchResult(Arg.Is<Match>(match => match.MatchResult.GetResult() == matchResult));
+    }
 
-        matchRepo.Received()
-                 .UpdateMatchResult(Arg.Is<Match>(match => match.MatchResult.GetResult() == "H"));
+    private void AfterActionDisplayResultShouldBe(EnumAction action, string expected)
+    {
+        var displayResult = _matchController.UpdateMatchResult(91, action);
+        displayResult.Should().Be(expected);
+    }
+
+    private void GivenMatchResultFromRepo(string matchResult)
+    {
+        _matchRepo.GetMatch(91)
+                  .Returns(new Match { Id = 91, MatchResult = new MatchResult(matchResult) });
     }
 }
